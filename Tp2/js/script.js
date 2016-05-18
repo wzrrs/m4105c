2562acf6babb2210634a5590880541ec2e78bfc0
Gaëtan Brysse <Gatan Brysse>
2016-05-18 15:02:56 +0200

    message

--- Tp2/js/script.js
+++ Tp2/js/script.js
@@ -1,26 +1,38 @@
-if (navigator.geolocation)
-    var watchId = navigator.geolocation.watchPosition(successCallback, errorCallback, {enableHighAccuracy:true});
-else
-    alert("Votre navigateur ne prend pas en compte la géolocalisation HTML5");
-    
-function successCallback(position){
-    alert("Latitude : " + position.coords.latitude + ", longitude : " + position.coords.longitude);
-};  
-
-function errorCallback(error){
-    switch(error.code){
-        case error.PERMISSION_DENIED:
-            alert("L'utilisateur n'a pas autorisé l'accès à sa position");
-            break;          
-        case error.POSITION_UNAVAILABLE:
-            alert("L'emplacement de l'utilisateur n'a pas pu être déterminé");
-            break;
-        case error.TIMEOUT:
-            alert("Le service n'a pas répondu à temps");
-            break;
+ function startWatch(){
+            if (navigator.geolocation)
+                var watchId = navigator.geolocation.watchPosition(successCallback,
+                                                                    errorCallback,
+                                                                    {enableHighAccuracy:true,
+                                                                    timeout:10000,
+                                                                    maximumAge:0});
+            else
+                alert("Votre navigateur ne prend pas en compte la géolocalisation HTML5");
         }
-};
-
-function stopWatch(){
-    navigator.geolocation.clearWatch(watchId);
-}   
+        
+        function stopWatch(){
+            navigator.geolocation.clearWatch(watchId);
+        }           
+        
+        function successCallback(position){
+            document.getElementById("lat").innerHTML = position.coords.latitude;
+            document.getElementById("long").innerHTML = position.coords.longitude;
+            document.getElementById("prec").innerHTML = position.coords.accuracy;
+            document.getElementById("alt").innerHTML = position.coords.altitude;
+            document.getElementById("precalt").innerHTML = position.coords.altitudeAccuracy;
+            document.getElementById("angle").innerHTML = position.coords.heading;
+            document.getElementById("speed").innerHTML = position.coords.speed;
+            document.getElementById("time").innerHTML = new Date(position.timestamp);
+        };  
+        function errorCallback(error){
+            switch(error.code){
+                case error.PERMISSION_DENIED:
+                    alert("L'utilisateur n'a pas autorisé l'accès à sa position");
+                    break;          
+                case error.POSITION_UNAVAILABLE:
+                    alert("L'emplacement de l'utilisateur n'a pas pu être déterminé");
+                    break;
+                case error.TIMEOUT:
+                    alert("Le service n'a pas répondu à temps");
+                    break;
+                }
+        };
