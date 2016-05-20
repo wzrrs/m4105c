f1083a368ae05474757f134d158c2c199b8774b6
Gaëtan Brysse <Gatan Brysse>
2016-05-20 09:23:06 +0200

    message

--- Tp2/js/script.js
+++ Tp2/js/script.js
@@ -62,38 +62,3 @@ if(navigator.geolocation){
   navigator.geolocation.getCurrentPosition(maPosition, erreurPosition,{enableHighAccuracy:true});
 }*/
 
-
-if ((window.DeviceMotionEvent) {
-  window.addEventListener('devicemotion', deviceMotionHandler, false);
-} else {
-  document.getElementById("dmEvent").innerHTML = "Not supported."
-}
-
-function deviceMotionHandler(eventData) {
-  var info, xyz = "[X, Y, Z]";
-
-  // Grab the acceleration from the results
-  var acceleration = eventData.acceleration;
-  info = xyz.replace("X", acceleration.x);
-  info = info.replace("Y", acceleration.y);
-  info = info.replace("Z", acceleration.z);
-  document.getElementById("moAccel").innerHTML = info;
-
-  // Grab the acceleration including gravity from the results
-  acceleration = eventData.accelerationIncludingGravity;
-  info = xyz.replace("X", acceleration.x);
-  info = info.replace("Y", acceleration.y);
-  info = info.replace("Z", acceleration.z);
-  document.getElementById("moAccelGrav").innerHTML = info;
-
-  // Grab the rotation rate from the results
-  var rotation = eventData.rotationRate;
-  info = xyz.replace("X", rotation.alpha);
-  info = info.replace("Y", rotation.beta);
-  info = info.replace("Z", rotation.gamma);
-  document.getElementById("moRotation").innerHTML = info;
-
-  // // Grab the refresh interval from the results
-  info = eventData.interval;
-  document.getElementById("moInterval").innerHTML = info;       
-}
